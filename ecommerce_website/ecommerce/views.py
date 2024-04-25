78fdba02ec0a4b59986a87c71497481dfd8e2f23
Kennedy <kennedycassiday@gmail.com>
2024-04-25 11:57:22 -0700

    created view, url, and html page for product list

--- ecommerce_website/ecommerce/views.py
+++ ecommerce_website/ecommerce/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from.models import Category, Product, Order, OrderItem
 
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'ecommerce/product_list.html', {'products': products})
