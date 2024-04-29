f796e9df15fa68168ed5386be18d1140bcb46913
Kennedy <kennedycassiday@gmail.com>
2024-04-29 15:04:20 -0700

    created view and url for add to cart feature

--- ecommerce_website/ecommerce/urls.py
+++ ecommerce_website/ecommerce/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
     product_list,
     product_detail,
+    add_to_cart,
 )
 
 urlpatterns = [
     path('', product_list, name='product_list'),
     path("<int:product_id>/", product_detail, name="product_detail"),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
 ]
--- ecommerce_website/ecommerce/views.py
+++ ecommerce_website/ecommerce/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from.models import Category, Product, Order, OrderItem
 
 # Create your views here.
@@ -16,3 +16,11 @@ def product_detail(request, product_id):
         'product_detail': product,
     }
     return render(request, 'ecommerce/product_detail.html', context)
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    order, created = Order.objects.get_or_create(user=request.user, is_ordered=False)
+    order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+    order_item.quantity += 1
+    order_item.save()
+    return redirect('product_list')
